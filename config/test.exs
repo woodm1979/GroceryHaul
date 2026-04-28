57b4f869137b7bf6d7904b8e4367ba2e7575e5c7
woodm1979 <woodm1979@gmail.com>
2026-04-28 14:24:09 -0600

    fix: Use port 5432 for Postgres per architectural decision

--- config/test.exs
+++ config/test.exs
@@ -9,7 +9,7 @@ config :grocery_haul, GroceryHaul.Repo,
   username: "postgres",
   password: "postgres",
   hostname: "localhost",
-  port: 5433,
+  port: 5432,
   database: "grocery_haul_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
