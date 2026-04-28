0542ee6ee74aba8b182c553516e4e59440eb44d6
woodm1979 <woodm1979@gmail.com>
2026-04-28 14:42:40 -0600

    simplify: merge prod Endpoint config, strip boilerplate comments, cache _build in CI

--- lib/grocery_haul/application.ex
+++ lib/grocery_haul/application.ex
@@ -1,6 +1,4 @@
 defmodule GroceryHaul.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
@@ -13,20 +11,13 @@ defmodule GroceryHaul.Application do
       GroceryHaul.Commanded.Application,
       {DNSCluster, query: Application.get_env(:grocery_haul, :dns_cluster_query) || :ignore},
       {Phoenix.PubSub, name: GroceryHaul.PubSub},
-      # Start a worker by calling: GroceryHaul.Worker.start_link(arg)
-      # {GroceryHaul.Worker, arg},
-      # Start to serve requests, typically the last entry
       GroceryHaulWeb.Endpoint
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: GroceryHaul.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
-  # Tell Phoenix to update the endpoint configuration
-  # whenever the application is updated.
   @impl true
   def config_change(changed, _new, removed) do
     GroceryHaulWeb.Endpoint.config_change(changed, removed)
